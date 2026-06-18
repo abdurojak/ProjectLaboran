a3753830147283b3570ab5ff2813ebc8c9f2ae11
MasRicardo <masrc632@gmail.com>
2026-06-18 17:20:10 +0700

    fix: update borrowing tests for borrower fields

--- apps/peminjaman/tests.py
+++ apps/peminjaman/tests.py
@@ -3,23 +3,25 @@ from datetime import date
 from django.test import TestCase
 from django.urls import reverse
 
-from apps.inventaris.models import Barang
+from apps.inventaris.models import Barang, Lokasi
 from .models import PeminjamanAlat
 
 
 class PeminjamanViewsTests(TestCase):
     def setUp(self):
+        self.lokasi = Lokasi.objects.create(nama_lokasi='Gudang A')
         self.barang = Barang.objects.create(
             nama='Mikroskop',
             kode_barang='LAB-001',
             jumlah=10,
-            lokasi='Gudang A',
+            lokasi=self.lokasi,
             kondisi='baik',
         )
         self.peminjaman = PeminjamanAlat.objects.create(
             barang=self.barang,
             nama_peminjam='Andi Pratama',
-            kelas_tujuan='X IPA 1',
+            nim='2201001',
+            no_hp='081234567890',
             jumlah=2,
             tanggal_pinjam=date(2026, 6, 18),
             tanggal_kembali=date(2026, 6, 20),
@@ -37,4 +39,5 @@ class PeminjamanViewsTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Andi Pratama')
-
+        self.assertContains(response, '2201001')
+        self.assertContains(response, '081234567890')
