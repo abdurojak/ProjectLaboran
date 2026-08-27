e7e83ee56317b787a22ca7b10690c87e30fea0cd
abdurojak <abdurojak27@gmail.com>
2026-08-27 16:02:39 +0700

    fix: scope production URLs to LabHub prefix

--- project_laboran/test_deployment.py
+++ project_laboran/test_deployment.py
@@ -1,3 +1,9 @@
+import json
+import os
+import subprocess
+import sys
+
+from django.conf import settings
 from django.test import SimpleTestCase
 from django.test.utils import override_script_prefix
 from django.urls import reverse
@@ -16,3 +22,45 @@ class HealthCheckTests(SimpleTestCase):
 
         self.assertEqual(reverse('health'), '/labhub/health/')
         self.assertEqual(response.status_code, 200)
+
+    @override_script_prefix('/labhub/')
+    def test_prefixed_dashboard_redirects_to_prefixed_login(self):
+        response = self.client.get('/', SCRIPT_NAME='/labhub')
+
+        self.assertEqual(
+            response.headers['Location'],
+            '/labhub/pengguna/login/?next=/labhub/',
+        )
+
+    def test_production_url_settings_follow_script_prefix(self):
+        environment = os.environ.copy()
+        environment.update({
+            'DEBUG': 'True',
+            'FORCE_SCRIPT_NAME': '/labhub',
+            'USE_SQLITE_FOR_TESTS': 'True',
+        })
+        environment.pop('MEDIA_URL', None)
+        command = (
+            'import json; '
+            'from django.conf import settings; '
+            'print(json.dumps({'
+            '"static": settings.STATIC_URL, '
+            '"media": settings.MEDIA_URL, '
+            '"session_cookie": settings.SESSION_COOKIE_PATH, '
+            '"csrf_cookie": settings.CSRF_COOKIE_PATH'
+            '}))'
+        )
+
+        output = subprocess.check_output(
+            [sys.executable, '-c', command],
+            cwd=os.fspath(settings.BASE_DIR),
+            env=environment,
+            text=True,
+        )
+
+        self.assertEqual(json.loads(output), {
+            'static': '/labhub/static/',
+            'media': '/labhub/media/',
+            'session_cookie': '/labhub/',
+            'csrf_cookie': '/labhub/',
+        })
